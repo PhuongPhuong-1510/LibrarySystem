f9ac93451e0ba97ae264b295e85b346f26eee95c
leducanhtai <161910484+leducanhtai@users.noreply.github.com>
2024-11-03 15:57:16 +0700

    dbconnecttion

---
+++ src/dataBase/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package dataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/checkData";
+    private static final String USER = "root";
+    private static final String PASSWORD = "13092005";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection conn = getConnection();
+        if (conn != null) {
+            System.out.println("Kết nối thành công!");
+        } else {
+            System.out.println("Kết nối thất bại.");
+        }
+    }
+}
