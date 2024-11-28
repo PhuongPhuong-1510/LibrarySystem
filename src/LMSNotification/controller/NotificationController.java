8d3c93b276cc671a07cd88563351f3a70bf0b39f
Thanqnv <161539907+Thanqnv@users.noreply.github.com>
2024-11-28 07:21:18 +0700

    cmt

--- src/LMSNotification/controller/NotificationController.java
+++ src/LMSNotification/controller/NotificationController.java
@@ -9,26 +9,48 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+
+/**
+ * Lớp điều khiển thông báo, quản lý các sự kiện liên quan đến giao diện NotificationView.
+ */
 public class NotificationController implements ActionListener, MouseListener {
     private NotificationView notificationView;
 
+    /**
+     * Khởi tạo NotificationController với một NotificationView.
+     *
+     * @param notificationView giao diện thông báo cần điều khiển.
+     */
     public NotificationController(NotificationView notificationView) {
         this.notificationView = notificationView;
         initializeListeners();
     }
 
+    /**
+     * Thêm các bộ lắng nghe sự kiện cho các thành phần trong giao diện.
+     */
     private void initializeListeners() {
         notificationView.getBackgroundPanel1().addMouseListener(this);
     }
 
+    /**
+     * Xử lý các hành động từ các sự kiện ActionEvent.
+     *
+     * @param e sự kiện được kích hoạt.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
     }
 
+    /**
+     * Xử lý sự kiện khi người dùng nhấn chuột vào panel.
+     *
+     * @param e sự kiện chuột được kích hoạt.
+     */
     @Override
     public void mouseClicked(MouseEvent e) {
         notificationView.toggleNotificationPanel();
-    }
+    } // Thay đổi trạng thái hiển thị của bảng thông báo.
 
     @Override
     public void mousePressed(MouseEvent e) {
@@ -38,11 +60,21 @@ public class NotificationController implements ActionListener, MouseListener {
     public void mouseReleased(MouseEvent e) {
     }
 
+    /**
+     * Xử lý sự kiện khi con trỏ chuột đi vào khu vực của panel.
+     *
+     * @param e sự kiện chuột được kích hoạt.
+     */
     @Override
     public void mouseEntered(MouseEvent e) {
         notificationView.getBackgroundPanel1().setBackground(new Color(60, 90, 130, 255));
     }
 
+    /**
+     * Xử lý sự kiện khi con trỏ chuột rời khỏi khu vực của panel.
+     *
+     * @param e sự kiện chuột được kích hoạt.
+     */
     @Override
     public void mouseExited(MouseEvent e) {
         notificationView.getBackgroundPanel1().setBackground(new Color(42, 71, 106, 255));
